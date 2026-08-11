81caac52053f6915c1e6db00a4b20508daf669ed
Lightningcurl <spasiotisanad@gmail.com>
2026-08-11 14:35:05 +0300

    fixed the loading button

--- components/Button.tsx
+++ components/Button.tsx
@@ -9,7 +9,7 @@ const variantStyles = {
   primary: "bg-[#1AF7B6] text-black",
   secondary: "bg-transparent text-[#1AF7B6] border border-[#1AF7B6]",
   destructive: "bg-red-600 text-white",
-  loading: "bg-blue-300 text-white cursor-wait",
+  loading: "bg-teal-100 text-white cursor-wait",
   disabled: "bg-gray-100 text-gray-400 cursor-not-allowed",
 };
 
