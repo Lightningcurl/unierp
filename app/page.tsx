cca831e412e19b4747f6522e7738f43935d64365
Lightningcurl <spasiotisanad@gmail.com>
2026-08-11 05:37:12 -0700

    added card component

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,6 @@
 import Image from "next/image";
 import Link from "next/link";
+import { Card } from "@/components/Card";
 
 export default function Home() {
   return (
@@ -13,6 +14,7 @@ export default function Home() {
           height={20}
           priority
         />
+        <Card category="Revenue">14820</Card>
         <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
           <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
             You are catching up{" "}
